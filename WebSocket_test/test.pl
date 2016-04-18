60ea286b64f02ba1430413260e90ce205296380c
Alexandre Paillier <pailli_a@epitech.eu>
2016-04-17 23:22:30 -0700

    Handles disconnection correctly, receives/sends multiples messages

--- WebSocket_test/test.pl
+++ WebSocket_test/test.pl
@@ -1,9 +1,10 @@
 use HTTP::Daemon;
 use Protocol::WebSocket;
+use POSIX;
 
 my $d = HTTP::Daemon->new(
     LocalAddr => '127.0.0.1',
-    LocalPort => 6666,
+    LocalPort => 80,
     ReuseAddr => 1
 ) || die 'Couldn\'t create the server';
 
@@ -21,18 +22,39 @@ while (my $c = $d->accept())
             {
                 $c->send_response(HTTP::Response->parse($hs->to_string()));
                 my $buf;
-                if ($c->recv($buf, 1024))
+                while ($c->sysread($buf, 2048))
                 {
                     my $frame = $hs->build_frame(buffer => $buf);
                     while (defined(my $message = $frame->next))
                     {
-                        print '-> ' . $message . "\n";
-                        $c->send($hs->build_frame(buffer => ':^)/')->to_bytes());                        
+                        if ($frame->is_close())
+                        {
+                            my $content = $hs->build_frame(type => 'close')->to_bytes();
+                            $c->syswrite($content, length($content));
+                            $c->close();
+                        }
+                        elsif ($frame->is_text())
+                        {
+                            print '-> ' . $message . "\n";
+                            ##
+                            my $content = $hs->build_frame(buffer => 'Hello there, I\'m the server :^)/')->to_bytes();
+                            my $oBytes = length($content);
+                            my $rBytes = $oBytes;
+                            print 'Sending frame of ' . $rBytes . " bytes\n";
+                            while ($rBytes > 0) # simulates partial writes
+                            {
+                                my $toSend = ceil($rBytes / 2);
+                                $c->syswrite($content, $toSend);
+                                print ' | ' if ($rBytes < $oBytes);
+                                print $toSend;
+                                $content = substr($content, $toSend);
+                                $rBytes -= $toSend;
+                            }
+                            print "\n";
+                        }
                     }
                 }
             }
         }
     }
-    $c->close();
-    undef($c);
 }
